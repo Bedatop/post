f1bc71685d19304c85f05ae93b8827afd758a831
Bedatop <98839707+Bedatop@users.noreply.github.com>
2023-05-15 17:43:18 +0500

    3

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let tov = document.querySelector('.tov')
 let cen = document.querySelector('.cen')
 let kat = document.querySelector('.kat')
 let btn = document.querySelector('.btn')
+let ul = document.querySelector('.ul')
 
 btn.addEventListener('click', function () {
-    let div = document.createElement('div');
-    div.classList.add('style');
-    div.innerHTML = tov.value;
+    let li = document.createElement('li');
+    li.classList.add('style');
+    li.innerHTML = tov.value;
+    li.appendChild(ul)
     })
